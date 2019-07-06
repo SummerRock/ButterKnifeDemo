1edb0361e5200706dd4a33e775ff73c9f3cd5943
yanxia <yan.xia@ihandysoft.com>
2019-07-06 14:21:48 +0800

    update

--- app/src/main/java/com/xk/butterknifedemo/MainActivity.java
+++ app/src/main/java/com/xk/butterknifedemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.xk.butterknifedemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,15 +12,17 @@ import com.xk.butterknifelib.InjectUtil;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.text)
     public TextView textView;
-    @BindView(R.id.text)
-    TextView textView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         InjectUtil.bind(this);
+    }
 
-        Toast.makeText(this, "xxx" + textView, Toast.LENGTH_SHORT).show();
+    public void showToast(View view) {
+        String toastString = textView == null ? "textView 为空！" : "textView 正常";
+        Toast.makeText(this, toastString, Toast.LENGTH_SHORT).show();
     }
 }
 
